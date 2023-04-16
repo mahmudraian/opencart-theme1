c7dcab5bf1cfb94bd86a54f1e1a1ad5651e927aa
AhmedDip <ahmedrb.dip@gmail.com>
2023-04-16 21:10:44 +0600

    best selling product module done

---
+++ catalog/language/en-gb/extension/module/best.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'Best Seller Products';
+
+$_['sub_title'] = 'Check Our Best Seller Products on ELMA website right now';
+
+// Text
+$_['text_tax']      = 'Ex Tax:';
