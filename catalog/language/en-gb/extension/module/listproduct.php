a908d77c0cffcae1b7a278b22146dcf1131e5c82
AhmedDip <ahmedrb.dip@gmail.com>
2023-04-17 14:57:53 +0600

    customer-stories-module added

---
+++ catalog/language/en-gb/extension/module/listproduct.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'Product list 1';
+$_['heading_title2'] = 'Product list 2';
+$_['heading_title3'] = 'Product list 3';
+
+// Text
+$_['text_tax']      = 'Ex Tax:';
