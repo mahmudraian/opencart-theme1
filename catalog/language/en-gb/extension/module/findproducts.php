ee364d534d116ba4194ddfd36cd8c959b7b06401
AhmedDip <ahmedrb.dip@gmail.com>
2023-04-17 10:45:38 +0600

    find-products-by-category done

---
+++ catalog/language/en-gb/extension/module/findproducts.php
@@ -0,0 +1,9 @@
+<?php
+
+$_['heading_title'] = 'Find Products By Category';
+$_['phone'] = 'Phones & PDAs';
+$_['camera'] = 'Cameras';
+$_['laptop'] = 'Laptops & Notebooks';
+$_['desktop'] = 'Desktop';
+$_['components'] = 'Components';
+$_['tablets'] = 'tablets';
